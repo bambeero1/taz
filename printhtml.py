474b6a94e30bc332a79db862c6c8cdd6aaad01b1
bambeero1 <47931114+bambeero1@users.noreply.github.com>
2023-05-22 23:56:43 +0300

    Update printhtml.py

--- printhtml.py
+++ printhtml.py
@@ -2,14 +2,17 @@ import sqlite3
 import pandas as pd
 import os
 
-if os.path.isfile('dresses.html'):
-    os.remove('dresses.html')
+if os.path.isfile('index.html'):
+    os.remove('index.html')
 
 # Connect to the SQLite database
 conn = sqlite3.connect('dresses.db')
 
 # Retrieve data from the "dresses" table
 df = pd.read_sql_query('SELECT * FROM dresses', conn)
+
+# Filter out rows with no sizes available
+df = df[df['sizes'] != 'No sizes available']
 
 # Sort the data by discount percentage
 df = df.sort_values(by='percentage', ascending=False)
@@ -18,16 +21,16 @@ df = df.sort_values(by='percentage', ascending=False)
 df['url_html'] = df['url'].apply(lambda x: '<a href="{0}">{0}</a>'.format(x))
 
 # Save the data as an HTML table with clickable URLs and Bootstrap styling
-with open('dresses.html', 'w') as f:
+with open('index.html', 'w') as f:
     f.write('<!DOCTYPE html>\n<html>\n<head>\n')
     f.write('<meta charset="UTF-8">\n')
     f.write('<title>Dresses</title>\n')
-    f.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">\n')
+    f.write('<link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.css">\n')
     f.write('<link rel="stylesheet" href="style.css">\n')
     f.write('</head>\n<body>\n')
     f.write('<div class="container">\n')
     f.write('<h1 class="text-center">Dresses</h1>\n')
-    f.write(df.to_html(escape=False, index=False, classes='table table-hover'))
+    f.write(df.to_html(escape=False, index=False, classes='table table-hover table-dark'))
     f.write('\n</div>\n')
     f.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>\n')
     f.write('<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>\n')
